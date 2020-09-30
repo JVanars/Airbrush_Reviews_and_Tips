a1f91fab90dd93b02d8721747e15b89a69b8747c
Jack Van Arsdall <jackvanarsdall@gmail.com>
2020-09-29 20:29:20 -0400

    changed navbar to mobile first approach

--- js/javascript.js
+++ js/javascript.js
@@ -1,9 +1,9 @@
-/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function hamburger() {
-    var x = document.getElementById("navLinks");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
   }
+}
