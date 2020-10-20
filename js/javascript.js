77b404c58723e24757b17dfc3d07212171944873
Jack Van Arsdall <jackvanarsdall@gmail.com>
2020-10-20 19:47:51 -0400

    updated form validation

--- js/javascript.js
+++ js/javascript.js
@@ -26,14 +26,36 @@ function readLess() {
   }
 }
 
+// function validateForm() {
+//   var x = document.forms["contactUs"]["user_name"].value;
+//   var xx = document.forms["contactUs"]["user_phone"].value;
+//   var y = document.forms["contactUs"]["user_email"].value;
+//   var z = document.forms["contactUs"]["user_message"].value;
+//   if (x == "" || y == "" || z == "" || xx == "") {
+//     alert("All present fields must be filled out.");
+//     return false;
+//   } else {
+//     alert("Thank you! Your message has been submitted and we will contact you soon!")
+//   }
+// }
+
+
 function validateForm() {
-  var x = document.forms["contactUs"]["user_name"].value;
-  var y = document.forms["contactUs"]["user_email"].value;
-  var z = document.forms["contactUs"]["user_message"].value;
-  if (x == "" || y == "" || z == "") {
+  var phone = document.getElementById("phone").value;
+  var name = document.getElementById("name").value;
+  var email = document.getElementById("mail").value;
+  var message = document.getElementById("message").value;
+  var x = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
+  if (name == "" || phone == "" || email == "" || message == "") {
     alert("All present fields must be filled out.");
     return false;
   } else {
-    alert("Thank you! Your message has been submitted and we will contact you soon!")
+    if (phone.match(x)) {
+      alert("Thank you! Your info has been received and we will contact you soon.");
+      return true;
+    } else {
+      alert("Not a valid Phone Number");
+      return false;
+    }
   }
 }
