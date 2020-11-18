c53c1a04feb485e20a2045c6daefde42725c996a
Jack Van Arsdall <jackvanarsdall@gmail.com>
2020-11-17 19:24:09 -0500

    image adjustments

--- js/reviews.js
+++ js/reviews.js
@@ -5,10 +5,10 @@ for (let i = 0; i < brushes.length; i++) {
     html += `
         <article class="reviews">
         <h2 class="reviewH2">${brush.brand} ${brush.model}</h2>
-        <img src="${brush.photo}" alt="${brush.brand} ${brush.model}" class="responsiveImg" class="reviewImg">
-        <h3>Mix: ${brush.mix}  ||  Feed-style: ${brush.feed}</h3>
-        <h3>Rating: ${brush.rating}</h3>
-        <h3>Price: $${brush.price}</h3>
+        <img src="${brush.photo}" alt="${brush.brand} ${brush.model}" class="responsiveImg reviewIMG">
+        <h3 class="review">Mix: ${brush.mix}  ||  Feed-style: ${brush.feed}</h3>
+        <h3 class="review">Rating: ${brush.rating}</h3>
+        <h3 class="review">Price: $${brush.price}</h3>
         <a href="order.html">ORDER NOW!</a>
         </article>
     `;
